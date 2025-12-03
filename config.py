fbefea6aebd3dac6c767bc72c2a6f6884630d1d2
unknown1-1 <chiew1201@gmail.com>
2025-12-04 01:29:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c1o622giw0nh.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "123967abc"
+customdb = "employee"
+custombucket = "chiewweiliang-employee"
+customregion = "us-east-1"
+
